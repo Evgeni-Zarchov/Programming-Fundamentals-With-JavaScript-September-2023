5de25c9ed1c6b4cc08dba490ec3505a62d3b7c79
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-01 18:19:02 +0300

    Create 07-triangleOfNumbers.js

---
+++ Basic-Syntax-Conditional-Statements-And-Loops-Exe/07-triangleOfNumbers.js
@@ -0,0 +1,8 @@
+function triangleOfNumbers(n) {
+    for (let num = 1; num <= n; num++) {
+        console.log(`${num} `.repeat(num));
+    }
+}
+triangleOfNumbers(3);
+triangleOfNumbers(5);
+triangleOfNumbers(6);
