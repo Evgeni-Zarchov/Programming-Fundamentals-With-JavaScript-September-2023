0ceea5b1a9e458ce9f6d951846a577aa8276cd74
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-01 18:38:22 +0300

    Create 02-concatenateNames.js

---
+++ Data-Types-and-Variables/02-concatenateNames.js
@@ -0,0 +1,6 @@
+function concNames(first, last, delimeter) {
+    console.log(`${first}${delimeter}${last}`);
+}
+concNames('John', 'Smith', '->');
+concNames('Jan', 'White', '<->');
+concNames('Linda', 'Terry', '=>');
