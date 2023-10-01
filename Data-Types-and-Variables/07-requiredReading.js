b47b832c13c9afcee85cb86c084a16c20efc96d0
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-01 18:38:35 +0300

    Create 07-requiredReading.js

---
+++ Data-Types-and-Variables/07-requiredReading.js
@@ -0,0 +1,6 @@
+function requiredReading(pages, pagesPerHour, days) {
+    let totalTime = pages / pagesPerHour / days;
+    console.log(totalTime);
+}
+requiredReading(212, 20, 2);
+requiredReading(432, 15, 4);
