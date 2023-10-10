64c693fdba114f59292dde106b8dd2fe2aff1771
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-10 16:59:18 +0300

    Create 01-smallestOfThreeNumbers.js

---
+++ Functions-Exe/01-smallestOfThreeNumbers.js
@@ -0,0 +1,9 @@
+function smallestNum(num1, num2, num3) {
+    let minNums = Math.min(num1, num2, num3);
+
+    console.log(minNums);
+}
+smallestNum(2, 5, 3);
+smallestNum(600, 342, 123);
+smallestNum(25, 21, 4);
+smallestNum(2, 2, 2);
