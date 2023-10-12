6f485aff5c8a634f6d8ac8b3dd19019db2183062
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-12 19:21:01 +0300

    Create 07-nXnMatrix.js

---
+++ Functions-Exe/07-nXnMatrix.js
@@ -0,0 +1,10 @@
+function nXnMatrix(n) {
+    for (let i = 0; i < n; i++) {
+        let currRow = `${n} `.repeat(n);
+        
+        console.log(currRow);
+    }
+}
+nXnMatrix(3);
+nXnMatrix(7);
+nXnMatrix(2);
