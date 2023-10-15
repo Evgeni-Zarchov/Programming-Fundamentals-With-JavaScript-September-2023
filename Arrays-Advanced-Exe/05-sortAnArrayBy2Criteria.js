63a2b0061c153b687e0e5e03feaee4ad5b7d7087
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-10-15 18:18:33 +0300

    Create 05-sortAnArrayBy2Criteria.js

---
+++ Arrays-Advanced-Exe/05-sortAnArrayBy2Criteria.js
@@ -0,0 +1,5 @@
+function sortBy2Criteria(arr) {
+    let sortedArr = arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
+    console.log(sortedArr.join('\n'));
+}
+sortBy2Criteria(['alpha', 'beta', 'gamma']);
