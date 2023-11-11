9911a5990a8919c7fab3b2b94f7a78f678d34cbd
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-11-11 16:17:05 +0200

    Create 04-storage.js

---
+++ Associative-Arrays/04-storage.js
@@ -0,0 +1,29 @@
+function shopStorage(arr) {
+    let storage = {};
+
+    for (let items of arr) {
+        let tokens = items.split(' ');
+
+        let product = tokens[0];
+        let quanity = Number(tokens[1]);
+
+        if (product in storage) {
+            storage[product] += quanity;
+        } else {
+            storage[product] = quanity;
+        }
+    }
+    let entries = Object.entries(storage);
+
+    for (let [prodcuts, quantity] of entries) {
+        console.log(`${prodcuts} -> ${quantity}`);
+    }
+}
+shopStorage(['tomatoes 10',
+    'coffee 5',
+    'olives 100',
+    'coffee 40']);
+shopStorage(['apple 50',
+    'apple 61',
+    'coffee 115',
+    'coffee 40']);
