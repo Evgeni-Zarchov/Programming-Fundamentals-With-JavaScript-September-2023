0a198352f12245e96ecb450947ebe9cb1389d3a1
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2023-11-19 09:50:04 +0200

    Create 02-substring.js

---
+++ Text-Processing/02-substring.js
@@ -0,0 +1,7 @@
+function stringManipulation(str, start, count) {
+    let end = start + count;
+
+    console.log(str.substring(start, end));
+}
+stringManipulation('ASentence', 1, 8);
+stringManipulation('SkipWord', 4, 7);
